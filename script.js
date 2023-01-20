bcff1bfc9b7c7949834a86ef43d7c00d2dfe95ec
Sarah <szconnan@gmail.com>
2023-01-20 11:44:26 +0100

    frontend ready

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ fetch('https://whatyearisit-backend-omega.vercel.app/year')
 	.then(response => response.json())
 	.then(data => {
 		if (data) {
-            document.querySelector('#year').textContent = 'Voyageur spatiaux-temporels, découvrez en quelle année vous êtes !' + 'Vous voici en' + data.year;
-				//document.querySelector('#year').innerHTML += `
-				//<p id="year">${data.year}</p>
-			//`;
+          //  document.querySelector('#year').textContent = 'Voyageur spatiaux-temporels, découvrez en quelle année vous êtes !' + 'Vous voici en' + data.year;
+				document.querySelector('#year').innerHTML += `
+				<p id="year">Voyageur spatiaux-temporels, découvrez en quelle année vous êtes ! Vous voici en : ${data.year}</p>
+			`;
 			}
 		}
 	);
